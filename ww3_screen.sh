aff4123449bb9740fb68ce2616810bcf4a2ba5ce
Dzmitry Sankouski <dsankouski@gmail.com>
2024-08-13 19:15:42 +0300

    fixup

--- ww3_screen.sh
+++ ww3_screen.sh
@@ -2,17 +2,20 @@
 
 USAGE=$(cat << EOF
 Usage: ./ww3_screen.sh <action [start|stop]> <game id>
+
+Before this command invocation, open the game and make
+sure it's window is active.
 EOF
 )
 
 action=$1
 game_id=$2
+window_id=$(xdotool getactivewindow)
 if [ "$action" = 'start' ]; then 
-  xdotool search --name "Conflict of Nations - World War 3 - Chromium" set_window --name "$game_id"
-  cat <<- EOF > /etc/cron.d/ww3-"$game_id"
-  */1 * * * * sh -c 'import -window "$game_id" ~/ww3/$game_id/screen-$(date +%y-%m-%d-%H:%M:%S).png'
-EOF
-elif [ "$action == 'stop' ]; then
+	cat <<- EOF > /etc/cron.d/ww3-"$game_id"
+	*/1 * * * * sh -c 'import -window "$window_id" ~/ww3/$game_id/screen-$(date +%y-%m-%d-%H:%M:%S).png'
+	EOF
+elif [ "$action" == 'stop' ]; then
   rm -rf /etc/cron.d/ww3-"$game_id"
 else
   echo $USAGE
